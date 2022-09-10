43d09194c6bf4ec9231585ec3540dad88b8ec935
Nicolas Mattia <nicolas@nmattia.com>
2022-09-10 22:00:39 +0200

    Initial commit

---
+++ app/src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { greet } from "lib";
+
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment} on:click={greet}>
+  count is {count}
+</button>
